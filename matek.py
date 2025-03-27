e35daf7b7a5f77766773776fa8de27b2d3e567eb
adrianhelllo <hetessyadrian@gmail.com>
2025-03-27 20:30:54 +0100

    Work in Progress 2

--- matek.py
+++ matek.py
@@ -2,11 +2,17 @@ import random
 import time
 
 playing = False
-min_operand, max_operand = 2, 12
-min_start, max_start = -15, 15
+MIN_OPERAND, MAX_OPERAND = 2, 12
+MIN_START, MAX_START = -15, 15
 operations = ['+', '-', '*', '/']
+diff = {
+    'easy': 5,
+    'medium': 6,
+    'hard': 8,
+    'nightmare': 12
+}
 op_done = 0
-oper_str = {
+oper_strs = {
     '+': 'add',
     '-': 'subtract',
     '*': 'multiply by',
@@ -15,19 +21,43 @@ oper_str = {
 begin = input("Welcome to my game! I will think of a number, and your task will be to figure out that number from the given operations. Enter anything here when you're ready: ")
 playing = True
 
-starting_num = random.randint(min_start, max_start)
+starting_num = random.randint(MIN_START, MAX_OPERAND)
 
-def rand_oper(num: int):
-    operand = random.randint(min_operand, max_operand)
-    current = num
-    op_done += 1
-    print("Alright, I will think of a number now...")
-    print("I have now decided on a number.")
+def print_decision(operation: str, operand: int, turn: int):
+    oper_str = oper_strs[operation]
+    
+    print(f"{'Next' if turn != 0 else 'First'}, I {oper_str} {operand}.")
 
-    op = random.choice(operations) 
-    current = eval(f"{current} {op} {operand}")
 
-    print(f"{'First' if op_done == 1 else 'Next'} let's {oper_str[op]}")
+def rand_oper(num: int, turn: int) -> int:
+    operation = random.choice(operations)
+    operand = random.randint(MIN_OPERAND, MAX_START)
+
+    print_decision(operation, operand, turn)
+
+    return eval(f"{num}{operation}{operand}")
+
+
+
+def ind_turn():
+    user_diff = input("Choose your difficulty // [easy, medium, hard, nightmare]: ")
+    
+    while user_diff not in diff.keys():
+        print("Please enter a valid difficulty.")
+        user_diff = input("Choose your difficulty // [easy, medium, hard, nightmare]: ")
+
+    for turn in range(diff[user_diff]):
+        if turn == 0:
+            turn_res = rand_oper(starting_num, turn)
+        else:
+            turn_res = rand_oper(turn_res, turn)
+
+    print(f"The result of the operations is {turn_res}. Figure out the original number!")
+    return turn_res
+
+
+def main():
+    pass
 
 
 
